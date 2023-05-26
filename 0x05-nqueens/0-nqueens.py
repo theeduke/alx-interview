307b10f4e051b6828d31b033a1e36a1c67c4813d
theeduke <oscarian528@gmail.com>
2023-05-26 14:21:42 -0700

    debug

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,70 +1,51 @@
 #!/usr/bin/python3
-"""
-program that solves the N queens problem.
-"""
+"""Solving N Queens with Backtracing"""
 import sys
 
-
-def nqueens(N):
+def nqueens(n, y, board):
     """
-    Solve the N Queens problem using backtracking.
-    Place N queens on an N by N board so that
-    no queens are attacking each other.
-    Return all possible solutions as a list of lists.
+    Method: nqueens - place n queens
+            on an n by n board so that
+            no queens are attacking any
+            others.
+    Parameters: n is an int that sets
+                board size and # of queens
+    Return: All possible solutions to
+            placement, in list of lists form
     """
-    solutions = []
-
-    def backtrack(y, board):
-        """
-        Recursive function to perform backtracking
-        """
-        if y == N:
-            solutions.append(list(board))
-            return
-
-        for x in range(N):
-            if is_safe(x, y, board):
-                board.append([x, y])
-                backtrack(y + 1, board)
-                board.pop()
-
-    def is_safe(x, y, board):
-        """
-        checks if placing queen on positon x,y is safe
-        """
+    for x in range(n):
+        hold = 0
         for q in board:
-            qx, qy = q
-            if x == qx or y == qy or abs(x - qx) == abs(y - qy):
-                return False
-        return True
-
-    backtrack(0, [])
-
-    return solutions
-
-
+            if x == q[1]:
+                hold = 1
+                break
+            if y - x == q[0] - q[1]:
+                hold = 1
+                break
+            if x - q[1] == q[0] - y:
+                hold = 1
+                break
+        if hold == 0:
+            board.append([y, x])
+            if y != n - 1:
+                nqueens(n, y + 1, board)
+            else:
+                print(board)
+            del board[-1]
 def main():
-    """
-    validates command line arguments
-    """
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
     try:
-        N = int(sys.argv[1])
-    except ValueError:
-        print("N must be a number")
+        n = int(sys.argv[1])
+    except Exception:
+        print('N must be a number')
         sys.exit(1)
-    if N < 4:
+    if n < 4:
         print("N must be at least 4")
         sys.exit(1)
 
-    queens_solutions = nqueens(N)
-    for solution in queens_solutions:
-        for row, col in solution:
-            print(f"[{row}, {col}]")
-        print()
-
+    nqueens(n, 0, [])
 
 if __name__ == '__main__':
     main()
